6798c676c0ab7378f70cab18ed4222e373a8067f
JoeLuciano <joeluciano@outlook.com>
2022-01-30 19:11:32 -0500

    Fixed mobile nav

--- src/components/pageComponents/mobilenav/MobileNav.js
+++ src/components/pageComponents/mobilenav/MobileNav.js
@@ -30,14 +30,12 @@ const navItem = {
 
 const MobileHeaderLinks = ({ navItems }) => {
   return (
-    <motion.div
-      className={styles['mobile-header-items']}
-      variants={navItemsVariant}>
+    <motion.div className={styles.mobileHeaderItems} variants={navItemsVariant}>
       {navItems.map((item) => (
         <Link
           key={item}
           to={`/${item.toLowerCase()}`}
-          className={styles['mobile-header-item']}>
+          className={styles.mobileHeaderItem}>
           <motion.h3
             variants={navItem}
             whileHover={{
@@ -51,17 +49,25 @@ const MobileHeaderLinks = ({ navItems }) => {
   );
 };
 
-const headerBackground = {
-  open: (height = 100) => ({
-    clipPath: `circle(${height * 2 + 200}px at 50% 90%)`,
+const mobileNavVariant = {
+  offScreen: { x: '10rem' },
+  onScreen: { x: 0 },
+};
+
+const posX = `calc(var(--nav-width) - var(--button-position-right) - var(--button-width)/2)`;
+const posY = `calc(var(--nav-height) - var(--button-position-bottom) - var(--button-height)/2)`;
+
+const mobileNavViewVariant = {
+  open: {
+    clipPath: `circle(30rem at ${posX} ${posY})`,
     transition: {
       type: 'spring',
       stiffness: 20,
       restDelta: 2,
     },
-  }),
+  },
   closed: {
-    clipPath: 'circle(1.5rem at 50% 85%)',
+    clipPath: `circle(1.5rem at ${posX} ${posY})`,
     transition: {
       type: 'spring',
       stiffness: 400,
@@ -80,38 +86,9 @@ const Path = (props) => (
   />
 );
 
-const MenuToggle = ({ toggle }) => (
-  <motion.button
-    className={styles['mobile-header-button']}
-    onClick={() => toggle}>
-    <motion.svg width='23' height='23' viewBox='0 0 23 23'>
-      <Path
-        variants={{
-          closed: { d: 'M 2 2.5 L 20 2.5' },
-          open: { d: 'M 3 16.5 L 17 2.5' },
-        }}
-      />
-      <Path
-        d='M 2 9.423 L 20 9.423'
-        variants={{
-          closed: { opacity: 1 },
-          open: { opacity: 0 },
-        }}
-        transition={{ duration: 0.1 }}
-      />
-      <Path
-        variants={{
-          closed: { d: 'M 2 16.346 L 20 16.346' },
-          open: { d: 'M 3 2.5 L 17 16.346' },
-        }}
-      />
-    </motion.svg>
-  </motion.button>
-);
-
-export const MobileNav = ({ isMobile = false, headerItems, userItems }) => {
-  const [isOpen, setIsOpen] = useState(true);
-  const [show, setShow] = useState(false);
+export const MobileNav = ({ isMobile, headerItems, userItems }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [show, setShow] = useState(true);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -122,24 +99,47 @@ export const MobileNav = ({ isMobile = false, headerItems, userItems }) => {
   }, []);
 
   return (
-    <motion.div
-      className={styles['mobile-header']}
-      initial={{ x: '50rem' }}
-      animate={{ x: 0, transition: { delay: 1, duration: 1 } }}>
-      {isMobile && show && (
+    isMobile && (
+      <motion.div
+        className={styles.mobileNav}
+        variants={mobileNavVariant}
+        initial='offScreen'
+        animate={show && 'onScreen'}>
         <motion.div
-          className={styles['mobile-header-background']}
-          variants={headerBackground}
+          className={styles.mobileNavContents}
+          variants={mobileNavViewVariant}
           animate={isOpen ? 'open' : 'closed'}>
+          <motion.button
+            className={styles.mobileNavButton}
+            onClick={() => setIsOpen((prev) => !prev)}>
+            <motion.svg width='23' height='23' viewBox='0 0 23 23'>
+              <Path
+                variants={{
+                  closed: { d: 'M 2 2.5 L 20 2.5' },
+                  open: { d: 'M 3 16.5 L 17 2.5' },
+                }}
+              />
+              <Path
+                d='M 2 9.423 L 20 9.423'
+                variants={{
+                  closed: { opacity: 1 },
+                  open: { opacity: 0 },
+                }}
+                transition={{ duration: 0.1 }}
+              />
+              <Path
+                variants={{
+                  closed: { d: 'M 2 16.346 L 20 16.346' },
+                  open: { d: 'M 3 2.5 L 17 16.346' },
+                }}
+              />
+            </motion.svg>
+          </motion.button>
           {isOpen && (
             <MobileHeaderLinks navItems={headerItems.concat(userItems)} />
           )}
-          <MenuToggle
-            styles={{ position: 'absolute' }}
-            toggle={() => setIsOpen((isOpen) => !isOpen)}
-          />
         </motion.div>
-      )}
-    </motion.div>
+      </motion.div>
+    )
   );
 };
