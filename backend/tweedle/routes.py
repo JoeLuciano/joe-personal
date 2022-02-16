1fe73d8becdf6f9783a23640f3a7e1192b48b90a
JoeLuciano <joeluciano@outlook.com>
2022-02-16 01:42:07 -0500

    Alternative spell checker

--- backend/tweedle/routes.py
+++ backend/tweedle/routes.py
@@ -1,22 +1,10 @@
 import os
-import enchant
+import csv
 from flask import Blueprint, request
 from flask_cors import cross_origin
 from backend.backend import Message
 
 tweedle = Blueprint('tweedle', __name__)
-
-broker = enchant.Broker()
-broker.describe()
-print(broker.describe())
-broker.list_languages()
-print(broker.list_languages())
-broker.list_dicts()
-print(broker.list_dicts())
-
-
-spellChecker = enchant.Dict()
-print(spellChecker.tag)
 
 
 @tweedle.route('/api/tweedle', methods=['POST'])
@@ -25,17 +13,33 @@ def getTweedleMatches():
     try:
         tweedleGuess = request.json
         dailyTweedle = os.getenv('DAILY_WORDLE')
-        guessResults = []
-        if spellChecker.check(tweedleGuess):
-            for (guess, actual) in zip(tweedleGuess, dailyTweedle):
-                if guess == actual:
-                    guessResults.append('match')
-                elif guess in dailyTweedle:
-                    guessResults.append('close')
-                else:
-                    guessResults.append('miss')
-            return Message.data(message="", data=guessResults), 200
+        if tweedleGuess == dailyTweedle:
+            return Message.data(message="", data=['match'] * 5), 200
         else:
-            return Message.msg('Not a word Tweedle Dum!'), 412
+            return process_wrong_guess(tweedleGuess, dailyTweedle)
     except Exception as e:
         return Message.error(e), 400
+
+
+def get_guess_results(tweedleGuess, dailyTweedle):
+    guessResults = []
+    for (guess, actual) in zip(tweedleGuess, dailyTweedle):
+        if guess == actual:
+            guessResults.append('match')
+        elif guess in dailyTweedle:
+            guessResults.append('close')
+        else:
+            guessResults.append('miss')
+    return guessResults
+
+
+def process_wrong_guess(tweedleGuess, dailyTweedle):
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    with open(os.path.join(__location__, 'worddb.csv'), 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            if tweedleGuess in row[0]:
+                guessResults = get_guess_results(tweedleGuess, dailyTweedle)
+                return Message.data(message="", data=guessResults), 200
+        return Message.msg('Not a word Tweedle Dum!'), 412
