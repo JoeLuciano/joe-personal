9ac7591cadf5d28384f0c2b301d98968cd168d6b
JoeLuciano <joeluciano@outlook.com>
2022-03-20 15:39:53 -0400

    Randomize wordle based on day

--- backend/tweedle/routes.py
+++ backend/tweedle/routes.py
@@ -3,8 +3,26 @@ import csv
 from flask import Blueprint, request
 from flask_cors import cross_origin
 from backend.backend import Message
+from datetime import date
+import pathlib
+import hashlib
 
 tweedle = Blueprint('tweedle', __name__)
+
+cwd = pathlib.Path(__file__).parent.resolve()
+tweedles_csv = os.path.join(cwd, "worddb.csv")
+total_tweedles = 12920
+
+today = date.today()
+daily_tweedle_index_str = str(today.year + today.month + today.day).encode()
+daily_tweedle_index_hex = hashlib.sha256(daily_tweedle_index_str).hexdigest()
+daily_tweedle_index = int(daily_tweedle_index_hex, 16) % total_tweedles + 1
+
+daily_tweedle = ''
+with open(tweedles_csv) as f:
+    daily_tweedle = (next((x for i, x in enumerate(csv.reader(f))
+                           if i == daily_tweedle_index), None))[0]
+daily_tweedle = daily_tweedle.split("'")[1]
 
 
 @tweedle.route('/api/tweedle', methods=['POST'])
@@ -12,11 +30,10 @@ tweedle = Blueprint('tweedle', __name__)
 def getTweedleMatches():
     try:
         tweedleGuess = request.json
-        dailyTweedle = os.getenv('DAILY_WORDLE')
-        if tweedleGuess == dailyTweedle:
+        if tweedleGuess == daily_tweedle:
             return Message.data(message="", data=['match'] * 5), 200
         else:
-            return process_wrong_guess(tweedleGuess, dailyTweedle)
+            return process_wrong_guess(tweedleGuess, daily_tweedle)
     except Exception as e:
         return Message.error(e), 400
 
@@ -34,9 +51,7 @@ def get_guess_results(tweedleGuess, dailyTweedle):
 
 
 def process_wrong_guess(tweedleGuess, dailyTweedle):
-    __location__ = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    with open(os.path.join(__location__, 'worddb.csv'), 'rt') as f:
+    with open(tweedles_csv, 'rt') as f:
         reader = csv.reader(f, delimiter=',')
         for row in reader:
             if tweedleGuess in row[0]:
