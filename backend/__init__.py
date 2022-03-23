c10ff831d5ba015aa40dc5992f1aac6ab4dd13eb
JoeLuciano <joeluciano@outlook.com>
2022-03-22 21:59:26 -0400

    Migrated db

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-# from flask_mail import Mail
 from backend.config import Config
 from flask_cors import CORS
 import os
@@ -18,7 +17,6 @@ bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-# mail = Mail()
 limiter = Limiter(key_func=get_remote_address)
 
 s3 = boto3.client("s3", aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
@@ -39,7 +37,6 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-    # mail.init_app(app)
     limiter.init_app(app)
 
     from backend.users.routes import users
@@ -51,9 +48,5 @@ def create_app(config_class=Config):
     app.register_blueprint(posts)
     app.register_blueprint(files)
     app.register_blueprint(tweedle)
-    # from flaskblog.main.routes import main
-    # from flaskblog.errors.handlers import errors
-    # app.register_blueprint(main)
-    # app.register_blueprint(errors)
 
     return app
